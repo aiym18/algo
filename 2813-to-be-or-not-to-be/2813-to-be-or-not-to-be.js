40629f2ea2da122e65e051c12c3a4cc3591cf879
aiym18 <160493792+aiym18@users.noreply.github.com>
2024-11-27 12:20:45 +0600

    Time: 35 ms (99.68%), Space: 49272000 (9.40%) - LeetHub

---
+++ 2813-to-be-or-not-to-be/2813-to-be-or-not-to-be.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+function expect(val) {
+  return {
+    toBe(expected) {
+      if (val !== expected) throw new Error("Not Equal");
+      return true;
+    },
+    notToBe(expected) {
+      if (val === expected) throw new Error("Equal");
+      return true;
+    }
+  };
+}
+
+
+/*
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
