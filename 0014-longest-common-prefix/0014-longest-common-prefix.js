468f0f21f6d6b092019b50faa4be6303aa361209
aiym18 <160493792+aiym18@users.noreply.github.com>
2025-02-17 20:16:24 +0600

    Time: 4 ms (30.93%), Space: 49656000 (50.70%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -3,16 +3,17 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-    if (strs.length === 0) return ""; 
+    if (strs.length === 0) return "";
     
-    let prefix = strs[0]; 
-    
-    for (let i = 1; i < strs.length; i++) {
-        while (strs[i].indexOf(prefix) !== 0) {
-            prefix = prefix.substring(0, prefix.length - 1); 
-            if (prefix === "") return ""; 
+    for (let i = 0; i < strs[0].length; i++) {
+        const char = strs[0][i];
+        
+        for (let j = 1; j < strs.length; j++) {
+            if (i >= strs[j].length || strs[j][i] !== char) {
+                return strs[0].substring(0, i);
+            }
         }
     }
     
-    return prefix;
+    return strs[0];
 };
